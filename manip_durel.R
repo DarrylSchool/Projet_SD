557dd591840da3b28e23cb4562053538bb54f75a
Darryl <Darryl.Kemgne-Nasah@etu.univ-grenoble-alpes.fr>
2024-01-17 10:39:46 +0100

    ajout de commande dans manip

--- manip_durel.R
+++ manip_durel.R
@@ -9,3 +9,10 @@ plot(X1[,1:10])
 
 X1 <- X[, -which(colnames(X) == "ID")]
 boxplot(split(X1$radius1,X1$Diagnosis), main="raduis1")
+
+install.packages("corrplot")
+library(corrplot)
+correlation_matrix <- cor(X1[,1:10])
+corrplot(correlation_matrix, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
+diagnosis_counts <- table(donnees$Diagnosis)
+pie(diagnosis_count, labels = paste(names(diagnosis_counts), "(", round(prop.table(diagnosis_counts) * 100, 1), "%)"), main = "Diagnosis Distribution")
