b76b532dd06bb4b011d87c5e6964f5f44a44ca5a
Durel63 <eteuboudurel@gmail.com>
2024-01-17 11:33:38 +0100

    Boxplot_feature1

--- manip_durel.R
+++ manip_durel.R
@@ -1,6 +1,11 @@
-,
+
 X1 <- X[, -which(colnames(X) == "Diagnosis")]
 X1 <- X1[, -which(colnames(X1) == "ID")]
 boxplot(X1)
 
 plot(X1[,1:10])
+
+
+
+X1 <- X[, -which(colnames(X) == "ID")]
+boxplot(split(X1$radius1,X1$Diagnosis), main="raduis1")
