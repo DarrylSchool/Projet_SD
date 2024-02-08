0910a5239bb0e906a6ab36f5bc00b0c17f591c3c
Durel63 <eteuboudurel@gmail.com>
2024-02-08 13:14:30 +0100

    Nouveau mat corr

--- manip_durel.R
+++ manip_durel.R
@@ -28,3 +28,19 @@ boxplot(split(X1$radius1,X1$Diagnosis), main="raduis1")
 # Suppression de plusieurs colonnes
 columns_to_remove <- c("Diagnosis", "Column2", "Column3")
 X1 <- X1[, -which(colnames(X1) %in% columns_to_remove)]
+
+
+# Histogramme
+install.packages("ggplot2")
+library(ggplot2)
+
+zone <- ggplot(X1, aes(x = radius))
+zone + geom_histogram(aes( fill = Diagnosis),alpha = 0.7) + 
+  xlim(0, 100) + geom_density() + 
+  ggtitle("Densité empirique de raduis1") + 
+  xlab("radius1") + 
+  ylab("densité de Kernel") + 
+  guides(fill=guide_legend(
+    title.position="left",label.position="right",label.hjust=0.5,direction="horizontal")
+  ) + 
+  theme(legend.position="bottom")  
