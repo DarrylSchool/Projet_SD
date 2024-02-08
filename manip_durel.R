6dd5a6b9d9c7d15d34cd603142d14b9f713338ff
Durel63 <eteuboudurel@gmail.com>
2024-02-08 21:40:28 +0100

    Heatmap

--- manip_durel.R
+++ manip_durel.R
@@ -1,47 +1,52 @@
 # Charger les données
-X <- read.csv("wdbc.csv")
-X
+dataset <- read.csv("wdbc.csv")
+dataset
 
 #Summary
-summary(X)
+summary(dataset)
 
 # Retirer l'ID et le diagnostic
-X1 <- X[, -which(colnames(X) == "Diagnosis")]
-X1 <- X1[, -which(colnames(X1) == "ID")]
+dataset1 <- dataset[, -which(colnames(dataset) == "Diagnosis")]
+dataset1 <- dataset1[, -which(colnames(dataset1) == "ID")]
 
 # Matrice de corrélation
 install.packages("corrplot")
 library(corrplot)
-correlation_matrix <- cor(X1[,1:10])
-corrplot(correlation_matrix, method = "color", type = "upper", tl.col = "black", tl.srt = 45)
+library(RColorBrewer)
 
-# Boxplot 
+correlation_matrix <- cor(as.matrix(dataset1))
 
-boxplot(X1)
+# Visualize as a heat map with correlation coefficients
+corrplot(correlation_matrix, method='color', type='upper', col=brewer.pal(n = 8, name = 'RdBu'),
+         tl.col='black', tl.srt=45, addCoef.col = 'black', number.cex = 0.7)
 
-plot(X1[,1:10])
+# Bodatasetplot 
+
+bodatasetplot(dataset1)
+
+plot(dataset1[,1:10])
 
 
-# Boxplot 
+# Bodatasetplot 
 
-X1 <- X[, -which(colnames(X) == "ID")]
-boxplot(split(X1$radius1,X1$Diagnosis), main="raduis1")
+dataset1 <- dataset[, -which(colnames(dataset) == "ID")]
+bodatasetplot(split(dataset1$radius1,dataset1$Diagnosis), main="raduis1")
 
 
 # Suppression de plusieurs colonnes
 columns_to_remove <- c("Diagnosis", "Column2", "Column3")
-X1 <- X1[, -which(colnames(X1) %in% columns_to_remove)]
+dataset1 <- dataset1[, -which(colnames(dataset1) %in% columns_to_remove)]
 
 
 # Histogramme
 install.packages("ggplot2")
 library(ggplot2)
 
-zone <- ggplot(X1, aes(x = radius))
+zone <- ggplot(dataset1, aes(dataset = radius))
 zone + geom_histogram(aes( fill = Diagnosis),alpha = 0.7) + 
-  xlim(0, 100) + geom_density() + 
+  datasetlim(0, 100) + geom_density() + 
   ggtitle("Densité empirique de raduis1") + 
-  xlab("radius1") + 
+  datasetlab("radius1") + 
   ylab("densité de Kernel") + 
   guides(fill=guide_legend(
     title.position="left",label.position="right",label.hjust=0.5,direction="horizontal")
